31919918f18ce40cbf810f60aa95c55b475ba5c7
NDimish <nathanmani@outlook.com>
2022-12-09 15:48:10 +0000

    form working

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,32 @@
 """Forms of the project."""
-
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
+from things.models import Thing
 # Create your forms here.
+
+class PostForm(forms.Form):
+
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(max_length=120)
+    quantity = forms.IntegerField(
+        validators=[MinValueValidator(0),MaxValueValidator(50)]
+    )
+#
+    class Meta:
+        fields=(
+
+            "name",
+            "description",
+            "quantity",
+        )
+
+    def save(self, commit=True):
+
+
+        user1 = Thing.objects.create_user(
+        name = self.cleaned_data.get('name'),
+        description = self.cleaned_data.get('description'),
+        quantity = self.cleaned_data.get('quantity'),
+        )
+
+        return user1
--- things/views.py
+++ things/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from things.forms import PostForm
 
 def home(request):
+    form = PostForm(request.POST or None)
     return render(request, 'home.html', {'form': form})
